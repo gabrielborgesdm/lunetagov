f84022f2d3f4785f4b6e0162dc6469b9197cc1f5
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2020-09-19 19:58:16 -0300

    App Name Changed

--- lunetagovapp/src/App.js
+++ lunetagovapp/src/App.js
@@ -1,23 +1,13 @@
 import React, { useEffect } from 'react';
-import {SafeAreaView, Linking, Dimensions } from 'react-native';
+import {SafeAreaView } from 'react-native';
 import { WebView } from 'react-native-webview';
-import {check, PERMISSIONS, request} from 'react-native-permissions'
 
 export default function App(){
-    useEffect(()=>{
-        console.log("alo")
-        request(PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE).then((result) => {
-            console.log(result)
-          });
-    }, [])
 
-    function handleUrlWithZip(result){
-        console.log(result)
-    }
     return (
         <SafeAreaView style={{flex: 1}}>
             <WebView 
-                source={{ uri: 'http://192.168.25.4:80/lunetagov/site/consultas.php' }} 
+                source={{ uri: 'https://lunetagov.com.br/site/' }} 
                 overScrollMode="content"
                 scrollEnabled={false}
                 useWebKit={true}
@@ -26,18 +16,7 @@ export default function App(){
                 cacheEnabled={true}
                 allowFileAccess={true}
                 domStorageEnabled={true}
-                startInLoadingState={true}
-                allowUniversalAccessFromFileURLs={true}
-                allowFileAccessFromFileURLs={true}
-                mixedContentMode={'always'}
-                allowsLinkPreview={true}
-                onFileDownload={({ nativeEvent: { downloadUrl } }) => {
-                    console.log("alo", downloadUrl)
-                  }}
-                allowingReadAccessToURL={true}
             />
-          
-            
         </SafeAreaView>
     )
 }
